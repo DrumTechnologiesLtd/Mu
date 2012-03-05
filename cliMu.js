6086f41ce3b590e40c61e71967d658fb71239d47
Mike Bardzinski <mike.bardzinski@netdev.co.uk>
2012-03-05 13:03:51 +0000

    Changed the wrapper to make it more suitable for command-line use

--- cliMu.js
+++ cliMu.js
@@ -1,31 +1,21 @@
-var sys = require('sys');
+#!/usr/bin/env node
+// Simple command-line wrapper to Mu
+//
+// Takes a template and a context / hash file and outputs to stdout
+
+var sys = require('sys'); // sys has been renamed to util in node 0.6
 var fs = require('fs');
 var Mu = require('./lib/mu');
 
+main(process.argv.slice(2));
 
-
-/*
- * Include some instructions for when I forget how this works.
- */
-function usage(message) {
-  usageLines = [
-  	"usage: cliMu <templateFile> <contextFile>",
-  	"\ttemplateFile: the template file to use.", 
-  	"\tcontextFile: filename containing the data to template, or other hash to use with the template.",
-  	"\nTemplated output is send to stdout.",
-  	];
-  	
-  	if (message) {
-  		sys.puts("\n** Error: "+message);
-  		sys.puts("");
-  	}
-  	
-  	usageLines.forEach(function (item) { sys.puts(item); });
-  	if (message) {
-  		process.exit(1);
-  	}
-  	
-  	process.exit(0);
+function main(argv) {
+	if (argv && argv.length == 2) {
+		renderWrapper(argv[0], argv[1]);
+	}
+	else {
+		usage("Incorrect number of arguments.");
+	}
 }
 
 function renderWrapper(templateFile, sourceFile) {
@@ -47,12 +37,23 @@ function renderWrapper(templateFile, sourceFile) {
 
 }
 
-if (process.argv.length != 4) {
-	usage("Incorrect number of arguments.");
-}
-
-renderWrapper(process.argv[2], process.argv[3]);
-
-
-
+function usage(message) {
+  usageLines = [
+  	"usage: cliMu <templateFile> <contextFile>",
+  	"\ttemplateFile: the template file to use.", 
+  	"\tcontextFile: filename containing the data to template, or other hash to use with the template.",
+  	"\nTemplated output is send to stdout.",
+  	];
   	
+  	if (message) {
+  		sys.puts("\n** Error: "+message);
+  		sys.puts("");
+  	}
+  	
+  	usageLines.forEach(function (item) { sys.puts(item); });
+  	if (message) {
+  		process.exit(1);
+  	}
+  	
+  	process.exit(0);
+}
